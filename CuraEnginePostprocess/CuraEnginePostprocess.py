e508d4f20f17006c8402dab9caadd1afd1718583
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-07-19 12:43:22 +0200

    Make the Python plugin a bit more robust
    CURA-10806

--- CuraEnginePostprocess/CuraEnginePostprocess.py
+++ CuraEnginePostprocess/CuraEnginePostprocess.py
@@ -1,22 +1,29 @@
 # Copyright (c) 2023 UltiMaker
 # Cura is released under the terms of the LGPLv3 or higher.
 
-
+import os
+import stat
 import platform
 from pathlib import Path
-from UM.i18n import i18nCatalog
 
+from UM.i18n import i18nCatalog
+from UM.Logger import Logger
 from cura.BackendPlugin import BackendPlugin
 
 catalog = i18nCatalog("cura")
 
-
 class CuraEnginePostprocess(BackendPlugin):
     def __init__(self):
         super().__init__()
-        self._plugin_command = [self.binaryPath()]
+        if not self.binaryPath().exists():
+            Logger.error(f"Could not find CuraEnginePostprocess binary at {self.binaryPath().as_posix()}")
+        if platform.system() != "Windows":
+            st = os.stat(self.binaryPath())
+            os.chmod(self.binaryPath(), st.st_mode | stat.S_IEXEC)
+
+        self._plugin_command = [self.binaryPath().as_posix()]
         self._supported_slots = [101]  # ModifyPostprocess SlotID
 
-    def binaryPath(self) -> str:
+    def binaryPath(self) -> Path:
         ext = ".exe" if platform.system() == "Windows" else ""
-        return Path(__file__).parent.joinpath(platform.machine(), platform.system(), f"curaengine_plugin_postprocess{ext}").resolve().as_posix()
+        return Path(__file__).parent.joinpath({ "AMD64": "x86_64", "x86_64": "x86_64" }.get(platform.machine()), platform.system(), f"curaengine_plugin_postprocess{ext}").resolve()
--- CuraEnginePostprocess/__init__.py
+++ CuraEnginePostprocess/__init__.py
@@ -1,14 +1,25 @@
 # Copyright (c) 2023 UltiMaker
 # Cura is released under the terms of the LGPLv3 or higher.
 
-from . import CuraEnginePostprocess
+import platform
 
-from UM.i18n import i18nCatalog
-catalog = i18nCatalog("cura")
+if platform.machine() in ["AMD64", "x86_64"]:
+    from . import CuraEnginePostprocess
 
+    from UM.i18n import i18nCatalog
+    catalog = i18nCatalog("cura")
 
-def getMetaData():
-    return {}
+    def getMetaData():
+        return {}
 
-def register(app):
-    return { "backend_plugin": CuraEnginePostprocess.CuraEnginePostprocess() }
+    def register(app):
+        return { "backend_plugin": CuraEnginePostprocess.CuraEnginePostprocess() }
+else:
+    from UM.Logger import Logger
+    Logger.error("CuraEnginePostprocess plugin is only supported on x86_64 systems")
+
+    def getMetaData():
+        return {}
+
+    def register(app):
+        return {}
